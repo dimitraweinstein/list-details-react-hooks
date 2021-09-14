29f75863f8da5e1e4968ed102bee48c7c5c86f80
Dimitra Weinstein <drenee.weinstein@gmail.com>
2021-09-14 16:45:32 -0700

    updated test to look for loading state, and built loading state into list container component

--- src/containers/RickAndMortyList.jsx
+++ src/containers/RickAndMortyList.jsx
@@ -1,7 +1,17 @@
 import React, { Component } from 'react';
 
 export default class RickAndMortyList extends Component {
+  state = {
+    loading: true,
+  };
+
   render() {
+    const { loading } = this.state;
+
+    if (loading) {
+      return <h1>Loading...</h1>;
+    }
+
     return <h1>Hello!</h1>;
   }
 }
--- src/containers/RickAndMortyList.test.jsx
+++ src/containers/RickAndMortyList.test.jsx
@@ -5,5 +5,7 @@ import RickAndMortyList from './RickAndMortyList';
 describe('Rick And Morty List Container', () => {
   it('should display a list of characters on a page', async () => {
     render(<RickAndMortyList />);
+
+    screen.getByText('Loading...');
   });
 });
