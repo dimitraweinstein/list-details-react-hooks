e5a75a44065ed238daf4c0269db5bd3df33ff816
Dimitra Weinstein <drenee.weinstein@gmail.com>
2021-09-14 16:40:17 -0700

    added rick and morty container to appjsx file now rendering correctly on localhost

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
+import RickAndMortyList from '../../containers/RickAndMortyList';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return <RickAndMortyList />;
 }
