87eac23e9c96f3c911bba9c2472ab3c6dddbbd03
Dimitra Weinstein <drenee.weinstein@gmail.com>
2021-09-14 20:12:08 -0700

    built container component and added react router dom import to app-jsx

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import RickAndMortyList from '../../containers/RickAndMortyList';
 
 export default function App() {
-  return <h1>Hello</h1>;
+  return <RickAndMortyList />;
 }
---
+++ src/containers/RickAndMortyList.jsx
@@ -0,0 +1,20 @@
+import React, { useState, useEffect, } from 'react';
+import CharacterList from '../components/characters/CharacterList';
+import { fetchCharacters } from '../services/rickAndMortyApi';
+
+const RickAndMortyList = () => {
+  const [loading, setLoading] = useState(true);
+  const [characters, setCharacters] = useState([]);
+  
+  useEffect(() => (
+    fetchCharacters()
+      .then((characters) => setCharacters(characters))
+      .finally(() => setLoading(false))
+  ), []);
+
+  if(loading) return <h1>Loading...</h1>;
+  
+  return <CharacterList characters={characters}/>;
+};
+
+export default RickAndMortyList;
