8c51cd659e4f162c03a0ae94c19f17b45b38eeb0
Dimitra Weinstein <drenee.weinstein@gmail.com>
2021-09-15 11:12:57 -0700

    set loading state in detail component and test is now passing for detail

--- src/containers/RickAndMortyDetailList.jsx
+++ src/containers/RickAndMortyDetailList.jsx
@@ -3,7 +3,9 @@ import Character from '../components/characters/Character';
 import { fetchOneCharacter } from '../services/rickAndMortyApi';
 
 const RickAndMortyDetailList = () => {
-  return <h1>Hello!</h1>;
+  const [ loading, setLoading ] = useState(true);
+  
+  if(loading) return <h1>Loading...</h1>;
 };
 
 export default RickAndMortyDetailList;
--- src/containers/RickAndMortyDetailList.test.jsx
+++ src/containers/RickAndMortyDetailList.test.jsx
@@ -1,2 +1,12 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import RickAndMortyDetailList from './RickAndMortyDetailList';
+// import { MemoryRouter } from 'react-router';
+
+describe('Rick And Morty Detail View Container', () => {
+  it('should display one character with details', async () => {
+    render(<RickAndMortyDetailList />);
+
+    screen.getByText('Loading...');
+  });
+});
